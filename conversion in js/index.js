65cd64e7c216725c893fc640db604a7db9cbf727
Muhammad Awais <awaisiqbal78692110@gmail.com>
2021-12-22 17:00:27 +0500

    Update index.js

--- conversion in js/index.js
+++ conversion in js/index.js
@@ -22,5 +22,15 @@ console.log(i.toString());
 let stri = Number("3456");
 stri = Number("3456d");
 stri = Number(false);
-stri = String([1,2,3,,4,5,6,7])
+stri = String([1, 2, 3, , 4, 5, 6, 7]);
 console.log(stri, typeof stri);
+
+// parseInt and parseFloat
+let numb = parseFloat("34.12345");
+console.log(numb.toFixed(5), typeof numb);
+
+// .................................................................................
+console.log("Welcome to coercion");
+let myStr = "awais";
+let myNum = "iqbal";
+console.log(myStr + " "+ myNum);
