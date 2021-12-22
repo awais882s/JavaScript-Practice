aaec9158579575a1d2b7eb860e90f5d6140abe6b
Muhammad Awais <awaisiqbal78692110@gmail.com>
2021-12-22 16:19:24 +0500

    diff operations

--- conversion in js/index.js
+++ conversion in js/index.js
@@ -1,5 +1,9 @@
 // type conversion and type coercion
 console.log("Welcome to conversion");
 let myVar;
-myVar = 34;
-console.log(myVar);
+myVar = String(34);
+console.log(myVar, typeof myVar);
+let booleanVar = String(true);
+console.log(booleanVar, typeof booleanVar);
+const date = String(new Date());
+console.log(date, typeof date);
