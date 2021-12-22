e53e4d12f18697ff03072731feff590980fa57ce
Muhammad Awais <awaisiqbal78692110@gmail.com>
2021-12-22 16:36:38 +0500

    arrays length

--- conversion in js/index.js
+++ conversion in js/index.js
@@ -7,3 +7,12 @@ let booleanVar = String(true);
 console.log(booleanVar, typeof booleanVar);
 const date = String(new Date());
 console.log(date, typeof date);
+// array
+const arr = String([1, 2, 3, 4, 5]);
+console.log(arr, arr.length, typeof arr);
+const myArr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+console.log(myArr, myArr.length, typeof myArr);
+const myArr1 = String([1, 2, 3, 4, 5, 6, 7, 8, 9]);
+
+console.log(myArr1, myArr1.length, typeof myArr1);
