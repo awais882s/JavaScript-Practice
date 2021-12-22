77f84e31671d2a8a70d9ca5ee8919abde0081915
Muhammad Awais <awaisiqbal78692110@gmail.com>
2021-12-22 14:46:33 +0500

    Update index.js

--- data types/index.js
+++ data types/index.js
@@ -1,3 +1,9 @@
 const name = "awais";
 console.log("My string is " + name);
 console.log("My Data type is " + typeof name);
+// numbers
+const marks = 882;
+console.log("Data type is" + " " + typeof marks);
+// boolen
+const isDriver = true;
+console.log("Data type is " + typeof isDriver);
