77877c58e39b8f30b16c0f8056c24979cac6629f
Muhammad Awais <awaisiqbal78692110@gmail.com>
2021-12-23 10:13:38 +0500

    diff operations in js

--- Template Literals in js/index.js
+++ Template Literals in js/index.js
@@ -14,3 +14,10 @@ console.log(html.toLocaleUpperCase());
 //
 
 console.log(html2[0]);
+console.log(html.lastIndexOf("<"));
+console.log(html2.lastIndexOf("S"));
+console.log(html.charAt(1));
+console.log(html.endsWith("para"));
+console.log(html.endsWith("soni"));
+console.log(html.includes("gfd"));
+console.log(html.substring(1,3));
