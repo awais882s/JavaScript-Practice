9fcfb23e7af2a05dd6e9556257136c84e2b68af9
Muhammad Awais <awaisiqbal78692110@gmail.com>
2021-12-23 09:51:52 +0500

    template literals

---
+++ Template Literals in js/index.js
@@ -0,0 +1,12 @@
+console.log("Template literals");
+const name = "awais";
+const greeting = "Good morning";
+console.log(greeting + " " + name);
+let html;
+html = "<h1>this is heading</h1>" + "<p>this is para</p>";
+
+html = html.concat("love","awais"," ","soni");
+console.log(html);
+html2 = "AWAISSONI"
+console.log(html2.length);
+console.log(html.toLocaleUpperCase());
