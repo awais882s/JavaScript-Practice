fce7c3ff317433ad8e728088b0c5296e3b8cc142
Muhammad Awais <awaisiqbal78692110@gmail.com>
2021-12-23 09:55:18 +0500

    index

--- Template Literals in js/index.js
+++ Template Literals in js/index.js
@@ -5,8 +5,12 @@ console.log(greeting + " " + name);
 let html;
 html = "<h1>this is heading</h1>" + "<p>this is para</p>";
 
-html = html.concat("love","awais"," ","soni");
+html = html.concat("love", "awais", " ", "soni");
 console.log(html);
-html2 = "AWAISSONI"
+html2 = "AWAISSONI";
 console.log(html2.toLocaleLowerCase());
 console.log(html.toLocaleUpperCase());
+
+//
+
+console.log(html2[0]);
