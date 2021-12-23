88fb35dd564295efdb8aae5499105ef6898bf4d1
Muhammad Awais <awaisiqbal78692110@gmail.com>
2021-12-23 14:57:06 +0500

    arrays functions

---
+++ Arrays and Objects/index.js
@@ -0,0 +1,13 @@
+console.log("Arrays and Objects");
+const marks = [23, 41, 45, 68, 89, 54, 22];
+const fruits = ["apple", "mango", "pineapple", "bannana"];
+const mixed = ["str", 89, [34, 35]];
+
+const arr = new Array(23, 123, "orange");
+console.log(arr);
+console.log(mixed);
+console.log(marks);
+console.log(fruits[0]);
+console.log(marks.includes("mango"));
+console.log(arr.length);
+console.log(Array.isArray(arr));
