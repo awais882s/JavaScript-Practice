a0c3273ffd8d3bec2887201da55d42053a8c3020
Muhammad Awais <awaisiqbal78692110@gmail.com>
2021-12-24 16:36:26 +0500

    if and else conditions

---
+++ If and else/index.js
@@ -0,0 +1,9 @@
+console.log("Welcome to lectures if and else");
+const age = 65;
+if (age == 19) {
+  console.log("Age is 19");
+} else if (age == 65) {
+  console.log("age is 65 done");
+} else {
+  console.log("Age is not 19");
+}
