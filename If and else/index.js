f09f2ba323f4e62dc6d9364cc1939d5237c91b89
Muhammad Awais <awaisiqbal78692110@gmail.com>
2021-12-24 17:03:47 +0500

    variable checks

--- If and else/index.js
+++ If and else/index.js
@@ -2,9 +2,17 @@ console.log("Welcome to lectures if and else");
 const age = 65;
 if (age != 19) {
   console.log("Age is 19");
-} 
+}
 if (age !== 65) {
   console.log("age is not 65 ");
 } else {
   console.log("Age is not 19");
+}
+
+// const vari = 65;
+if ( typeof vari !=="undefined") {
+  console.log("vari is defined");
+}
+else{
+    console.log("vari is not defined");
 }
