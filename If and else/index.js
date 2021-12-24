87faae2d18974efdc41517102530a6efb4cfb7b2
Muhammad Awais <awaisiqbal78692110@gmail.com>
2021-12-24 18:33:13 +0500

    ternary operators

--- If and else/index.js
+++ If and else/index.js
@@ -1,5 +1,5 @@
 console.log("Welcome to lectures if and else");
-const age = 65;
+const age = 45;
 if (age != 19) {
   console.log("Age is 19");
 }
@@ -9,10 +9,18 @@ if (age !== 65) {
   console.log("Age is not 19");
 }
 
-// const vari = 65;
-if ( typeof vari !=="undefined") {
+const vari = 65;
+if (typeof vari !== "undefined") {
   console.log("vari is defined");
+} else {
+  console.log("vari is not defined");
 }
-else{
-    console.log("vari is not defined");
+
+const doesDrive = true;
+if (doesDrive || age > 18) {
+  console.log("You can drive");
+} else {
+  console.log("you cannot drive");
 }
+// ternary operator
+console.log(age === 45 ? "age is 45" : "age is not 45");
