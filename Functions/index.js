1b971900268fa70b53127d01a5c00d4b2df22a54
Muhammad Awais <awaisiqbal78692110@gmail.com>
2021-12-25 14:59:48 +0500

    functions

--- Functions/index.js
+++ Functions/index.js
@@ -1,6 +1,18 @@
 // functions
 console.log("Function in js");
-let name = "skillF";
+let name1 = "skillF";
 console.log(
-  `Happy Birthday ${name} The older I get, the more I realize that I am still a little younger than you! Happy birthday.`
+  `Happy Birthday ${name1} The older I get, the more I realize that I am still a little younger than you! Happy birthday.`
 );
+// =====================================================
+console.log("With functions=========================================:");
+function greeting(name, thank = "Thanks You sir for wishings") {
+  console.log(
+    `Happy Birthday ${name} The older I get, the more I realize that I am still a little younger than you! Happy birthday.
+    ${thank}`
+  );
+}
+let name = "awais";
+let name2 = "lovers g";
+greeting(name, "Thanks a lot");
+greeting(name2, "Thanks a lover");
