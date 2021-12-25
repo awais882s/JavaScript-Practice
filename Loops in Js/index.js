1dfed58395de35793b02ffaf7432a6ff022b5b5d
Muhammad Awais <awaisiqbal78692110@gmail.com>
2021-12-25 14:29:47 +0500

    Update index.js

--- Loops in Js/index.js
+++ Loops in Js/index.js
@@ -9,7 +9,6 @@ for (let i = 0; i < 100; i++) {
   console.log(i);
 }
 
-
 // for (let i = 0; i < arr.length; i++) {
 //     const element = arr[i];
 //     console.log(element);
@@ -18,16 +17,18 @@ for (let i = 0; i < 100; i++) {
 // ========================================================================
 console.log("For loops in========================== js");
 let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-arr.forEach(function (index , index , array) {
-  console.log(index,index,array);
+arr.forEach(function (element, index, array) {
+  console.log(element, index, array);
 });
 let obj = {
-    name:"awais",
-    age:14,
-    type:"hacker",
-    os : "window",
-}
-console.log("object in js======================================================");
-for(let key in obj){
-    console.log(`The ${key} of object is ${obj[key]}`);
+  name: "awais",
+  age: 14,
+  type: "hacker",
+  os: "window",
+};
+console.log(
+  "object in js======================================================"
+);
+for (let key in obj) {
+  console.log(`The ${key} of object is ${obj[key]}`);
 }
