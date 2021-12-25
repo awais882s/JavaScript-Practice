b2866ac3b97041f5a2c6b084c8471a00a40b7785
Muhammad Awais <awaisiqbal78692110@gmail.com>
2021-12-25 14:36:09 +0500

    without function

---
+++ Functions/index.js
@@ -0,0 +1,6 @@
+// functions
+console.log("Function in js");
+let name = "skillF";
+console.log(
+  `Happy Birthday ${name} The older I get, the more I realize that I am still a little younger than you! Happy birthday.`
+);
