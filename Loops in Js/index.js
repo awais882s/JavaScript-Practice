08c22765770895c96ac95c3f45010ccb4291518a
Muhammad Awais <awaisiqbal78692110@gmail.com>
2021-12-25 11:01:02 +0500

    for loop

---
+++ Loops in Js/index.js
@@ -0,0 +1,10 @@
+console.log("loops in js");
+// General loops for, while, do while
+// let a = 34;
+// a += 1;
+// a++;
+// console.log("value of a is :", a);
+
+for (let i = 0; i < 100; i++) {
+  console.log(i);
+}
