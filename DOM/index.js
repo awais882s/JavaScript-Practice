455e7266bac14eb03f370c4227ab721091a8b447
Muhammad Awais <awaisiqbal78692110@gmail.com>
2021-12-27 16:29:22 +0500

    confirm function

--- DOM/index.js
+++ DOM/index.js
@@ -3,3 +3,5 @@ let a = window;
 // alert("Hi Awais Here");
 a = prompt("This will destroy your computer. Type your name=:");
 console.log(a);
+b = confirm("Are you sure you want to delete this page?");
+console.log(b);
