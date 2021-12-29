ca065cd149356f550586fd86908c5d3113b4f38f
Muhammad Awais <awaisiqbal78692110@gmail.com>
2021-12-29 10:18:40 +0500

    documents

--- Understanding DOM/index.js
+++ Understanding DOM/index.js
@@ -3,9 +3,13 @@ let a = document;
 a = document.all;
 // a = document.body;
 // a = document.forms;
-// a.foreach(function (element) {
+// Array.from(a).forEach(function (element) {
 //   console.log(element);
 // });
+
 // a = document.body;
 // a = document.forms[0];
-console.log(a);
+// console.log(a);
+b = document.links[0].href;
+// use document.images and document.scripts and print the list of images and scripts on the html pages
+console.log(b);
