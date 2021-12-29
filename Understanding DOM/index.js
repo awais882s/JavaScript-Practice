87a3a472b99aa77a6a56bc1baf04898994d80f83
Muhammad Awais <awaisiqbal78692110@gmail.com>
2021-12-29 10:11:04 +0500

    documents gets in js

---
+++ Understanding DOM/index.js
@@ -0,0 +1,11 @@
+console.log("Understanding DOM,s");
+let a = document;
+a = document.all;
+// a = document.body;
+// a = document.forms;
+// a.foreach(function (element) {
+//   console.log(element);
+// });
+// a = document.body;
+// a = document.forms[0];
+console.log(a);
