b2e2e64b029e94fcf58e0c162315b45b602a7900
Muhammad Awais <awaisiqbal78692110@gmail.com>
2021-12-29 22:20:15 +0500

    single elements selectors

--- DOM Selectors/index.js
+++ DOM Selectors/index.js
@@ -1,3 +1,10 @@
 console.log("Welcome To DOM,s Selectors");
 a = document.all;
 console.log(a);
+
+// elements selectors types
+//  1.multi elements selectors
+//  2.Single elements selectors
+
+let element = document.getElementById("myfirst");
+console.log(element);
