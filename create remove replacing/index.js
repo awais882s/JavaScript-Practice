b715c11e8676892863573bc51b5236b3f8886846
Muhammad Awais <awaisiqbal78692110@gmail.com>
2021-12-30 16:35:26 +0500

    in js class and id creates

--- create remove replacing/index.js
+++ create remove replacing/index.js
@@ -1,1 +1,8 @@
 console.log("create remove and replacing");
+let element = document.createElement("li");
+//  add a class names
+element.className = "childul";
+element.id = "created li";
+element.setAttribute("title", "my love");
+
+console.log(element);
