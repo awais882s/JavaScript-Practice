02a6af45ba2cf15b9c457164fa8f9b33c81d6341
Muhammad Awais <awaisiqbal78692110@gmail.com>
2021-12-30 22:23:29 +0500

    replace with , appendchilds

--- create remove replacing/my.js
+++ create remove replacing/my.js
@@ -1,5 +1,27 @@
 console.log("welcome to awais");
 let element = document.createElement("li");
-element.className = "my_list";
-element.id = "my_id_list";
+element.className = "mylist";
+element.id = "mylist";
+element.setAttribute("title", "my_love_and_lifeline");
+element.innerHTML = "<b>We never leave our,s</b>";
 console.log(element);
+let li = document.querySelector("#list_one");
+li.appendChild(element);
+console.log(li);
+
+// another way to add,s one
+let lists = document.querySelector(".new");
+
+let textNew = document.createTextNode("i am new nodes to add with");
+lists.appendChild(textNew);
+
+console.log(textNew);
+// any others,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,
+console.log("new+++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
+
+let heading = document.createTextNode("programming is the best practice");
+heading.className = "heagings";
+heading.id = "headings";
+console.log(heading);
+let life = document.querySelector(".life");
+life.replaceWith(heading);
