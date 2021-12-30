f48128f4d888be5fa06156c64d2157179ff40415
Muhammad Awais <awaisiqbal78692110@gmail.com>
2021-12-30 10:17:43 +0500

    chiildnodes and children

--- Traversing the DOM/index.js
+++ Traversing the DOM/index.js
@@ -1,1 +1,5 @@
 console.log("Traversing the DOM,s");
+let cont = document.querySelector(".no");
+cont = document.querySelector(".container");
+console.log(cont.childNodes);
+console.log(cont.children);
