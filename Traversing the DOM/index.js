bff9e35f8e77fb97f07110997c534386b527b347
Muhammad Awais <awaisiqbal78692110@gmail.com>
2021-12-30 10:29:57 +0500

    diff nodeActions

--- Traversing the DOM/index.js
+++ Traversing the DOM/index.js
@@ -1,5 +1,19 @@
 console.log("Traversing the DOM,s");
 let cont = document.querySelector(".no");
 cont = document.querySelector(".container");
-console.log(cont.childNodes);
-console.log(cont.children);
+
+let nodeName = cont.childNodes[1].nodeName;
+let nodeType = cont.childNodes[1].nodeType;
+console.log(nodeName);
+console.log(nodeType);
+// console.log(cont.childNodes);
+// console.log(cont.children);
+
+/*
+node type lists
+1.Element
+2.Attributes
+3.Text Node
+8.Comment
+9.document
+10.DocType */
