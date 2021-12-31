421319c55d8a8e9cf35651d5592431d3b31a32ee
Muhammad Awais <awaisiqbal78692110@gmail.com>
2021-12-31 22:05:14 +0500

    on mouse hover functions in js

--- Events and Events Objects/index.js
+++ Events and Events Objects/index.js
@@ -14,4 +14,18 @@ document.getElementById("heading").addEventListener("click", function (e) {
   console.log(variable);
 });
 
-// 
+// on mouse over functions==================================
+document.getElementById("heading").addEventListener("mouseover", function (e) {
+  console.log("you have click the headings awais");
+  //   alert("you open this website");
+  //   prompt("you open this website muhammad awais house");
+  //   location.href = "https://muhammad-awais-house.business.site/";
+  variable = e.target;
+  variable = e.target.className;
+  variable = Array.from(e.target.classList);
+  //   variable = e.target.id;
+  variable = e.offsetX;
+  variable = e.clientX;
+  variable = e.clientY;
+  console.log(variable);
+});
