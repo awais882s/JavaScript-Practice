3f8920240239ee7c89d97f9029b9084bfdc74122
Muhammad Awais <awaisiqbal78692110@gmail.com>
2021-12-31 17:02:40 +0500

    events on click

--- Events and Events Objects/index.js
+++ Events and Events Objects/index.js
@@ -1,7 +1,12 @@
 console.log("welcome to Events and Events objects");
-document.getElementById("heading").addEventListener("click", function () {
+document.getElementById("heading").addEventListener("click", function (e) {
   console.log("you have click the headings awais");
-  alert("you open this website");
-  prompt("you open this website muhammad awais house");
-  location.href = "https://muhammad-awais-house.business.site/";
+  //   alert("you open this website");
+  //   prompt("you open this website muhammad awais house");
+  //   location.href = "https://muhammad-awais-house.business.site/";
+  variable = e.target;
+  variable = e.target.className;
+  variable = Array.from(e.target.classList);
+  //   variable = e.target.id;
+  console.log(variable);
 });
