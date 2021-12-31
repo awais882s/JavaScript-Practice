ff6aff26488a6511af7c6063c0f3a95f57d1df9d
Muhammad Awais <awaisiqbal78692110@gmail.com>
2021-12-31 16:29:29 +0500

    add event listener

--- Events and Events Objects/index.js
+++ Events and Events Objects/index.js
@@ -1,1 +1,7 @@
 console.log("welcome to Events and Events objects");
+document.getElementById("heading").addEventListener("click", function () {
+  console.log("you have click the headings awais");
+  alert("you open this website");
+  prompt("you open this website muhammad awais house");
+  location.href = "https://muhammad-awais-house.business.site/";
+});
