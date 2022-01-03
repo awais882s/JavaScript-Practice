e75d0653008e37a2af7dd6c41c808707b08bcc59
Muhammad Awais <awaisiqbal78692110@gmail.com>
2022-01-03 14:02:37 +0500

    events on btns

--- Events and Events Objects/18.More On Events/index.js
+++ Events and Events Objects/18.More On Events/index.js
@@ -1,1 +1,7 @@
 console.log("more on events");
+let btn = document.getElementById("btn");
+addEventListener("click", func1);
+function func1(e) {
+  console.log("thanks", e);
+  e.preventDefault();
+}
