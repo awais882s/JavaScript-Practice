6fd1923eea8a0976911bcab0495501963235e13a
Muhammad Awais <awaisiqbal78692110@gmail.com>
2022-01-03 13:46:45 +0500

    events listner

--- Events and Events Objects/my.js
+++ Events and Events Objects/my.js
@@ -1,1 +1,10 @@
 console.log("buttons");
+document
+  .getElementById("btn-submit")
+  .addEventListener("mouseover", function (e) {
+    console.log("you have click the submit buttons");
+    // alert("this is buttons");
+    // let name = prompt("enter your lover name");
+    // console.log(name);
+    // location.href = "https://muhammad-awais-house.business.site/";
+  });
