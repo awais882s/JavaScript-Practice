d0bb368e08fff62eea7d96b8b39da143432c5907
Muhammad Awais <awaisiqbal78692110@gmail.com>
2022-01-03 14:08:54 +0500

    mousedown functions

--- Events and Events Objects/18.More On Events/index.js
+++ Events and Events Objects/18.More On Events/index.js
@@ -5,3 +5,13 @@ function func1(e) {
   console.log("thanks", e);
   e.preventDefault();
 }
+addEventListener("dblclick", func2);
+function func2(e) {
+  console.log("thanks its a double click", e);
+  e.preventDefault();
+}
+addEventListener("mousedown", func3);
+function func3(e) {
+  console.log("thanks its a mouse down functions", e);
+  e.preventDefault();
+}
