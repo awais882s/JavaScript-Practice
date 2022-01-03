ab721401c28a3ef951d7e1a1eac1ad1c69d7f64c
Muhammad Awais <awaisiqbal78692110@gmail.com>
2022-01-03 13:49:52 +0500

    Update my.js

--- Events and Events Objects/my.js
+++ Events and Events Objects/my.js
@@ -7,4 +7,12 @@ document
     // let name = prompt("enter your lover name");
     // console.log(name);
     // location.href = "https://muhammad-awais-house.business.site/";
+    variable = e.target;
+    variable = e.target.className;
+    variable = Array.from(e.target.classList);
+    // variable = e.target.id;
+    variable = e.offsetX;
+    variable = e.offsetY;
+    variable = e.clientX;
+    console.log(variable);
   });
