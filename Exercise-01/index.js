2d37568e1be1f6ec65869ae75526843ab8720e1d
Muhammad Awais <awaisiqbal78692110@gmail.com>
2022-01-04 23:08:11 +0500

    Update index.js

--- Exercise-01/index.js
+++ Exercise-01/index.js
@@ -1,9 +1,11 @@
 console.log("links");
-let str = "python";
+let str = "https://muhammad-awais-house.business.site/";
 let links = document.links;
 console.log(links);
 let href;
 Array.from(links).forEach(function (element) {
   href = element.href;
-  console.log(href);
+  if (href.includes(str)) {
+    console.log(href);
+  }
 });
