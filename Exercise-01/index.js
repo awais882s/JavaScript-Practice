9f4166bd0bd4e1d9476ea68654a2f6c8308c23e1
Muhammad Awais <awaisiqbal78692110@gmail.com>
2022-01-04 22:33:50 +0500

    links fetch

--- Exercise-01/index.js
+++ Exercise-01/index.js
@@ -1,1 +1,9 @@
 console.log("links");
+let str = "python";
+let links = document.links;
+console.log(links);
+let href;
+Array.from(links).forEach(function (element) {
+  href = element.href;
+  console.log(href);
+});
