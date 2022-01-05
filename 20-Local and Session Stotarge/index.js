6884e2c51d57d66f9bc7c57d6a46800061719b26
Muhammad Awais <awaisiqbal78692110@gmail.com>
2022-01-05 16:20:32 +0500

    local storages

--- 20-Local and Session Stotarge/index.js
+++ 20-Local and Session Stotarge/index.js
@@ -1,1 +1,6 @@
 console.log("local and session storage");
+localStorage.setItem("name", "awais");
+localStorage.setItem("name", "Lovers");
+localStorage.clear();
+let name = localStorage.getItem("name");
+console.log(name);
