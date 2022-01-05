e52a1513d4ba86227e3121e303fa353b63dd5f9a
Muhammad Awais <awaisiqbal78692110@gmail.com>
2022-01-05 21:30:01 +0500

    diff functionalities

--- My-Todo-App/app.js
+++ My-Todo-App/app.js
@@ -2,12 +2,29 @@ const list = document.getElementById("list");
 
 function addTodo() {
   var todo_item = document.getElementById("todo-item");
+
+  //  create li tags with text node
   var li = document.createElement("li");
   var liText = document.createTextNode(todo_item.value);
   li.appendChild(liText);
+
+  // create delete buttons
+  var delbtn = document.createElement("button");
+  var delText = document.createTextNode("Delete");
+  delbtn.setAttribute("class", "btn");
+  delbtn.setAttribute("onclick", "deleteItem(this)");
+  delbtn.appendChild(delText);
+  li.appendChild(delbtn);
+
   //   console.log(todo_item.value);
   list.appendChild(li);
 
   todo_item.value = "";
+
   console.log(li);
+}
+function deleteItem(e) {
+  console.log(e);
+  
+  
 }
