04c81962b79e769bfbdb323c5fcc11160238c8f9
Muhammad Awais <awaisiqbal78692110@gmail.com>
2022-01-05 16:30:08 +0500

    local storage

--- 20-Local and Session Stotarge/index.js
+++ 20-Local and Session Stotarge/index.js
@@ -1,6 +1,16 @@
 console.log("local and session storage");
+let impArray = ["adrak", "pyaz", "bhindi"];
+//  add key value pairs
 localStorage.setItem("name", "awais");
-localStorage.setItem("name", "Lovers");
-localStorage.clear();
+localStorage.setItem("name2", "Lovers");
+localStorage.setItem("sbzi", JSON.stringify(impArray));
+// localStorage.clear(); clear the entire values
+// retrieve the items from local storage
+
 let name = localStorage.getItem("name");
 console.log(name);
+// clear particular key
+localStorage.removeItem("name");
+
+sbzi = JSON.parse(localStorage.getItem("sbzi"));
+console.log(sbzi);
