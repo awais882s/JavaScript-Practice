e3e8675d53cea016520967b60746cbfcc0008f71
Muhammad Awais <awaisiqbal78692110@gmail.com>
2022-01-05 20:43:07 +0500

    lists

--- My-Todo-App/app.js
+++ My-Todo-App/app.js
@@ -3,8 +3,11 @@ const list = document.getElementById("list");
 function addTodo() {
   var todo_item = document.getElementById("todo-item");
   var li = document.createElement("li");
-  var liText = document.createTextNode("Awais");
+  var liText = document.createTextNode(todo_item.value);
   li.appendChild(liText);
-  console.log(todo_item.value);
+  //   console.log(todo_item.value);
+  list.appendChild(li);
+
+  todo_item.value = "";
   console.log(li);
 }
