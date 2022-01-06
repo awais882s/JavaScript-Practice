3d74528d876e52d145db6ad6dfc9b54cc5d1732e
Muhammad Awais <awaisiqbal78692110@gmail.com>
2022-01-06 14:39:23 +0500

    todo app

--- My-Todo-App/Again my Todo App create/index.js
+++ My-Todo-App/Again my Todo App create/index.js
@@ -1,1 +1,15 @@
 console.log("my TODO APP CREATE");
+
+// here we get list from html to js
+let list = document.getElementById("list");
+
+// add todo text functionss
+function addTodo() {
+  let input = document.getElementById("todo_input");
+  let li = document.createElement("li");
+  let liText = document.createTextNode(todo_input.value);
+  li.appendChild(liText);
+  // for empty input tags
+  todo_input.value = "";
+  list.appendChild(li);
+}
