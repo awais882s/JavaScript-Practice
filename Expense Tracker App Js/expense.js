84d9734cf762973b191add5ac3c617faa089d131
Muhammad Awais <awaisiqbal78692110@gmail.com>
2022-01-07 19:45:42 +0500

    javascript things get

---
+++ Expense Tracker App Js/expense.js
@@ -0,0 +1,32 @@
+var state = {
+  balance: 0,
+  income: 1200,
+  expense: 200,
+  transactions: [
+    {
+      name: "salary",
+      amount: 5000,
+      type: "income",
+    },
+    {
+      name: "Buy Grocery",
+      amount: 50,
+      type: "expense",
+    },
+    {
+      name: "Buy Guitar",
+      amount: 500,
+      type: "expense",
+    },
+  ],
+};
+// functions============================================
+var balanceEl = document.querySelector("#balance");
+var incomeEl = document.querySelector("#income");
+var expensEl = document.querySelector("#expense");
+function init() {
+  balanceEl.innerHTML = `${state.balance}`;
+  incomeEl.innerHTML = `${state.income}`;
+  expensEl.innerHTML = `${state.expense}`;
+}
+init();
