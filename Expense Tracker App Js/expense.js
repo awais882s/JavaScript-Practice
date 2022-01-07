3fd31dfc55155b764a4680e6b3eb147768ba06f5
Muhammad Awais <awaisiqbal78692110@gmail.com>
2022-01-07 20:09:15 +0500

    Update expense.js

--- Expense Tracker App Js/expense.js
+++ Expense Tracker App Js/expense.js
@@ -24,9 +24,26 @@ var state = {
 var balanceEl = document.querySelector("#balance");
 var incomeEl = document.querySelector("#income");
 var expensEl = document.querySelector("#expense");
+var transactionsEl = document.querySelector("#transaction");
 function init() {
   balanceEl.innerHTML = `${state.balance}`;
   incomeEl.innerHTML = `${state.income}`;
   expensEl.innerHTML = `${state.expense}`;
+
+  var transactionEl, containerEL, amountEL, item;
+  for (var i = 0; i < state.transactions.length; i++) {
+    item = state.transactions[i];
+    transactionEl = document.createElement("li");
+    transactionEl.append(item.name);
+    transactionEl.appendChild(transactionEl);
+    //   create a div elements
+    containerEl = document.createElement("div");
+    amountEl = document.createElement("span");
+    if (item.type === "income") {
+      amountEL.classList.add("income-amt");
+    } else if (item.type === "expense") {
+      amountEL.classList.add("expense-amt");
+    }
+  }
 }
 init();
