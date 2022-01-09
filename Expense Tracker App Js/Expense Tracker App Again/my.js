58c636ccce4b5d60b39fc1b7e0377631e665a4d0
Muhammad Awais <awaisiqbal78692110@gmail.com>
2022-01-09 21:45:14 +0500

    we create a div using js

--- Expense Tracker App Js/Expense Tracker App Again/my.js
+++ Expense Tracker App Js/Expense Tracker App Again/my.js
@@ -20,13 +20,38 @@ var state = {
     },
   ],
 };
-// create a init function()===============================================
+// create a init function()=================================================================================================
 var balanceEl = document.querySelector("#balance");
 var incomeEl = document.querySelector("#income");
 var expenseEl = document.querySelector("#expense");
+var transactionsEl = document.querySelector("#transaction");
 function init() {
   balanceEl.innerHTML = `$${state.balance}`;
   incomeEl.innerHTML = `$${state.income}`;
   expenseEl.innerHTML = `$${state.expense}`;
+  //   create a list of variables=====================================================================================
+  var transactionEl, containerEl, amountEl, item, btnEl;
+  //   for length of transaction check outside of for loop==================================================================
+  var tranlen = state.transactions.length;
+  for (var i = 0; i < tranlen; i++) {
+    item = state.transactions[i];
+    transactionEl = document.createElement("li");
+    transactionEl.append(item.name);
+    transactionsEl.appendChild(transactionEl);
+    // create a div and span element and also one button ===========================================================================================
+    containerEl = document.createElement("div");
+    amountEl = document.createElement("span");
+    if (item.type === "income") {
+      amountEl.classList.add("income-amt");
+    } else if (item.type === "expense") {
+      amountEl.classList.add("expense-amt");
+    }
+    amountEl.innerHTML = `$${item.amount}`;
+    containerEl.appendChild(amountEl);
+    btnEl = document.createElement("button");
+    btnEl.innerHTML = "X";
+    containerEl.appendChild(btnEl);
+    transactionEl.appendChild(containerEl);
+  }
 }
 init();
