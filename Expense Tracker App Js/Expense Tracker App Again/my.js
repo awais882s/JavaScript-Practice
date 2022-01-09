4df6ee26069b77eb497c208a32ec78c33fb695f6
Muhammad Awais <awaisiqbal78692110@gmail.com>
2022-01-09 23:22:09 +0500

    update states using js

--- Expense Tracker App Js/Expense Tracker App Again/my.js
+++ Expense Tracker App Js/Expense Tracker App Again/my.js
@@ -26,6 +26,30 @@ var incomeEl = document.querySelector("#income");
 var expenseEl = document.querySelector("#expense");
 var transactionsEl = document.querySelector("#transaction");
 function init() {
+  updateState();
+  render();
+}
+function updateState() {
+  var balance = 0,
+    income = 0,
+    expense = 0,
+    item;
+  var tranlen = state.transactions.length;
+  for (var i = 0; i < tranlen; i++) {
+    item = state.transactions[i];
+    if (item.type === "income") {
+      income = income + item.amount;
+    } else if (item.type === "expense") {
+      expense = expense + item.amount;
+    }
+  }
+  balance = income - expense;
+
+  state.balance = balance;
+  state.income = income;
+  state.expense = expense;
+}
+function render() {
   balanceEl.innerHTML = `$${state.balance}`;
   incomeEl.innerHTML = `$${state.income}`;
   expenseEl.innerHTML = `$${state.expense}`;
