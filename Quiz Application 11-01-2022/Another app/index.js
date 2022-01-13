3923663348c8419def1fb960c62e555a9b2df6cf
Muhammad Awais <awaisiqbal78692110@gmail.com>
2022-01-13 12:20:20 +0500

    js questions store in a variables

--- Quiz Application 11-01-2022/Another app/index.js
+++ Quiz Application 11-01-2022/Another app/index.js
@@ -1,1 +1,38 @@
 console.log("my quiz app");
+var questionArray = [
+    {
+        question: "Full form of RAM is?",
+        answer: "Random Access Memory",
+        options: [
+            "Random Access Memory",
+            "Draft Access Memory",
+            "Disk Access Memory",
+            "None of these",
+        ],
+    },
+    {
+        question: "FULL FORM OF : C P U?",
+        answer: "CENTRAL PROCESSING UNIT",
+        options: [
+            " CONTROL PROCESING UNIT",
+            "CENTRAL PROCESSING UNIT",
+            "CENTRAL PROCESS UNIT",
+            "None of these",
+        ],
+
+    },
+    {
+        question: "SEO stands for",
+        answer: " None of the above",
+        options: [
+            " Search Engine Optimum",
+            "Search Electronic Optimization",
+            "all of these",
+            "None of these",
+        ],
+
+    }
+
+
+
+];
