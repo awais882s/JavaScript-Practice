ec6dea1114982ffd1572dac056bc777177bc4edc
Muhammad Awais <awaisiqbal78692110@gmail.com>
2022-01-14 22:57:22 +0500

    create a text elements

--- 21-Creating Editable Div by Js/index.js
+++ 21-Creating Editable Div by Js/index.js
@@ -1,5 +1,19 @@
 console.log("Creating Editable Div Using JavaScript");
-let div_container = document.createElement("div");
-div_container.id = "createElem";
-div_container.innerHTML = 'Submit Text To Local Storage By Clicking on Text :';
-document.body.appendChild(div_container);
+// Part-1 :You Have to create a div and enter into the page.=========================================
+let createElem = document.createElement("div");
+createElem.id = "createElem";
+createElem.innerHTML = "Submit Text To Local Storage By Clicking on Text :";
+document.body.appendChild(createElem);
+
+// Part-2 :Whenever someone clicks on the div ,it should be converted into an editable item.==============================
+
+let getText = document.getElementById("createElem");
+console.log(getText);
+getText.addEventListener("dblclick", func1);
+function func1() {
+  let createInputElem = document.createElement("input");
+  createInputElem.id = "createinputElem";
+  createInputElem.type = "text";
+  createInputElem.innerText = "";
+  createElem.appendChild(createInputElem);
+}
