aab88d62656679426066855eaeb5e55c818602b8
Muhammad Awais <awaisiqbal78692110@gmail.com>
2022-01-14 22:45:12 +0500

    create a div by js

--- 21-Creating Editable Div by Js/index.js
+++ 21-Creating Editable Div by Js/index.js
@@ -1,1 +1,5 @@
 console.log("Creating Editable Div Using JavaScript");
+let div_container = document.createElement("div");
+div_container.id = "createElem";
+div_container.innerHTML = 'Submit Text To Local Storage By Clicking on Text :';
+document.body.appendChild(div_container);
