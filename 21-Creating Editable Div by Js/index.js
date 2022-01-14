e330b88bc7d51812a2f95352df0406f533ea891d
Muhammad Awais <awaisiqbal78692110@gmail.com>
2022-01-14 23:00:00 +0500

    Update index.js

--- 21-Creating Editable Div by Js/index.js
+++ 21-Creating Editable Div by Js/index.js
@@ -17,3 +17,28 @@ function func1() {
   createInputElem.innerText = "";
   createElem.appendChild(createInputElem);
 }
+
+// Part-1 :You Have to create a div and enter into the page.
+
+let createElem = document.createElement("div");
+createElem.id = "createElem";
+createElem.innerText = "Submit Text To Local Storage By Clicking on Text : ";
+
+document.body.appendChild(createElem);
+
+// Part-2 :Whenever someone clicks on the div ,it should be converted into an editable item.
+
+let getText = document.getElementById("createElem");
+console.log(getText);
+getText = addEventListener("dblclick", func1);
+
+function func1() {
+  let createinputElem = document.createElement("input");
+  createinputElem.id = "createinputElem";
+  createinputElem.type = "text";
+  createinputElem.innerText = "";
+  createElem.appendChild(createinputElem);
+}
+
+// Part-3 :and whenever user clicks away(blur). save the note into the local storage.===================
+
