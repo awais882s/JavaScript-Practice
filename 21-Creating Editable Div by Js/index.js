748a257ef5ea83aa4809c663e4c22741f0354239
Muhammad Awais <awaisiqbal78692110@gmail.com>
2022-01-14 23:07:22 +0500

    add event listeners

--- 21-Creating Editable Div by Js/index.js
+++ 21-Creating Editable Div by Js/index.js
@@ -18,27 +18,12 @@ function func1() {
   createElem.appendChild(createInputElem);
 }
 
-// Part-1 :You Have to create a div and enter into the page.
-
-let createElem = document.createElement("div");
-createElem.id = "createElem";
-createElem.innerText = "Submit Text To Local Storage By Clicking on Text : ";
-
-document.body.appendChild(createElem);
-
-// Part-2 :Whenever someone clicks on the div ,it should be converted into an editable item.
-
-let getText = document.getElementById("createElem");
-console.log(getText);
-getText = addEventListener("dblclick", func1);
-
-function func1() {
-  let createinputElem = document.createElement("input");
-  createinputElem.id = "createinputElem";
-  createinputElem.type = "text";
-  createinputElem.innerText = "";
-  createElem.appendChild(createinputElem);
-}
 
 // Part-3 :and whenever user clicks away(blur). save the note into the local storage.===================
 
+let getTextValue = document.getElementById("createinputElem");
+getTextValue.addEventListener("click", func2);
+function func2() {
+  let getValueof = createinputElem.value;
+  localStorage.setItem("Data Entered", getValueof);
+}
