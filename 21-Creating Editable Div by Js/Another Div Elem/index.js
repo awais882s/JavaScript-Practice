8b683cbb15f550abacaa7e663ef2f96b052b5d94
Muhammad Awais <awaisiqbal78692110@gmail.com>
2022-01-15 14:13:02 +0500

    event listeners in input value

--- 21-Creating Editable Div by Js/Another Div Elem/index.js
+++ 21-Creating Editable Div by Js/Another Div Elem/index.js
@@ -6,3 +6,21 @@ createElem.id = "createElem";
 createElem.innerHTML = `<h2>Submit text to local storage by clicking on the Text:</h2>`;
 document.body.appendChild(createElem);
 console.log(createElem);
+
+// part2===================================================/////////
+
+let getText = document.getElementById("createElem");
+getText.addEventListener("dblclick", function () {
+  let createInputElem = document.createElement("input");
+  createElem.id = "createInputElem";
+  createInputElem.type = "text";
+  createInputElem.innerText = "";
+  createElem.appendChild(createInputElem);
+  let getTextValue = document.getElementById("createInputElem");
+getTextValue.addEventListener("click", function () {
+  let getValueof = createInputElem.value;
+  localStorage.setItem("Data Entered", getValueof);
+});
+
+});
+// part3==============================================/////////
