e62aad0a6ead618f27dbbf48be850eeb40e01149
Muhammad Awais <awaisiqbal78692110@gmail.com>
2022-01-18 22:56:37 +0500

    event on add btn

--- 22-Project Add Notes/index.js
+++ 22-Project Add Notes/index.js
@@ -1,1 +1,18 @@
 console.log("Notes app");
+//  if user adds a note , add it to the local storage
+let addBtn = document.getElementById("addBtn");
+
+addBtn.addEventListener("click", function (e) {
+  let addTxt = document.getElementById("addTxt");
+  let notes = localStorage.getItem("notes");
+
+  if (notes == null) {
+    notesobj = [];
+  } else {
+    notesobj = JSON.parse(notes);
+  }
+  // notesobj.push(addTxt.value);
+  localStorage.setItem("notes", JSON.stringify(notesobj));
+  addTxt.value = "";
+  console.log(notesobj);
+});
