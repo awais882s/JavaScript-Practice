fc64f5632ad1b833f49dc1b2cd9d3a49fe8d9153
Muhammad Awais <awaisiqbal78692110@gmail.com>
2022-01-19 23:14:49 +0500

    diff math operations

--- 23-Math Object/index.js
+++ 23-Math Object/index.js
@@ -6,4 +6,23 @@ z = x + y;
 z = x * y;
 z = x - y;
 z = x / y;
+// exploring the math object
+z = Math;
+z = Math.PI;
+z = Math.E;
+// z = Math.round(5.4);
+// z = Math.floor(5.1);
+z = Math.ceil(5.3);
+z = Math.abs(-5.3);
+
 console.log(z);
+
+// 3
+// 2.8
+// 2
+// 1
+// 0
+// -1
+// -2
+// -2.7
+// -3
