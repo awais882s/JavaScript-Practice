0d6ebfa377eb4279e42d0680bd4a7ada8b3999db
Muhammad Awais <awaisiqbal78692110@gmail.com>
2022-01-19 23:31:43 +0500

    math functions

--- 23-Math Object/index.js
+++ 23-Math Object/index.js
@@ -15,9 +15,17 @@ z = Math.E;
 z = Math.ceil(5.3);
 z = Math.abs(-5.3);
 z = Math.sqrt(49);
-z = Math.pow(2,3);
-z = Math.min(2,3.364,49,989);
-z = Math.max(2,3.364,49,989);
+z = Math.pow(2, 3);
+z = Math.min(2, 3.364, 49, 989);
+z = Math.max(2, 3.364, 49, 989);
+z = Math.random();
+z = 100 * Math.random();
+z = Math.ceil(50 + (100 - 50) * Math.random());
+
+// a = (0,1);
+// a100 = a*100 = (0,100)
+// a10_100 = 10+a*(100-10)
+
 console.log(z);
 
 // 3
