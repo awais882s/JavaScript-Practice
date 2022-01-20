5fce78a3e1a4c7ab93c8df72170d48442d406458
Muhammad Awais <awaisiqbal78692110@gmail.com>
2022-01-20 21:59:04 +0500

    date formats

--- 24-Date and Time/index.js
+++ 24-Date and Time/index.js
@@ -1,3 +1,9 @@
 console.log("Date and Time In JS");
 let today = new Date();
 console.log(today);
+console.log(typeof today);
+// ==============================================
+let otherDate = new Date("09-05-2000 12:12:12");
+otherDate = new Date("june 09 2000");
+otherDate = new Date("05/09/2000");
+// console.log(otherDate);
