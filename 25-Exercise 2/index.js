b4d253b742ab735273bdb10d822af2bd3c46c32c
Muhammad Awais <awaisiqbal78692110@gmail.com>
2022-01-21 21:45:30 +0500

    stylings

--- 25-Exercise 2/index.js
+++ 25-Exercise 2/index.js
@@ -1,1 +1,9 @@
 console.log("Div editable");
+let divElem = document.createElement("div");
+let text = document.createTextNode("This is my element. click to edit it");
+
+divElem.setAttribute("id", "elem");
+divElem.setAttribute("style", "border:2px");
+let container = document.querySelector(".container");
+let first = document.getElementById("myfirst");
+console.log(divElem, container, first);
