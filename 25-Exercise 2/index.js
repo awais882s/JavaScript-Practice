242c35e20bcf845cf05f457a0824cf48106a891f
Muhammad Awais <awaisiqbal78692110@gmail.com>
2022-01-21 22:11:13 +0500

    add event on div an editable

--- 25-Exercise 2/index.js
+++ 25-Exercise 2/index.js
@@ -1,6 +1,7 @@
 console.log("Div editable");
 // create a new element
 let divElem = document.createElement("div");
+
 // add text in that created element
 let text = document.createTextNode("This is my element. click to edit it");
 divElem.appendChild(text);
@@ -8,11 +9,21 @@ divElem.setAttribute("class", "elem");
 divElem.setAttribute("id", "elem");
 divElem.setAttribute(
   "style",
-  "border:2px solid black; width:154px; margin:34px"
+  "border:2px solid black; width:180px; margin:30px; padding:10px"
 );
+// grab the main container
+
 let container = document.querySelector(".container");
 let first = document.getElementById("myfirst");
 // insert the element before element with id first
 
 container.insertBefore(divElem, first);
 console.log(divElem, container, first);
+// add event listener on the div
+divElem.addEventListener("click", function () {
+  let noTextAreas = document.getElementsByClassName("textarea").length;
+  if (noTextAreas == 0) {
+    let html = elem.innerHTML;
+    divElem.innerHTML = `<textarea class=" textarea form-control" id = "textarea" rows="3">${html}</textarea>`;
+  }
+});
