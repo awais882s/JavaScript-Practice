97adbc622adf05e0d03d688fe1f7467c49081e23
Muhammad Awais <awaisiqbal78692110@gmail.com>
2022-01-23 14:56:39 +0500

    html code

--- 25-Exercise 2/Again Exercise/index.js
+++ 25-Exercise 2/Again Exercise/index.js
@@ -2,7 +2,16 @@ console.log("div editable");
 let divElem = document.createElement("div");
 let text = document.createTextNode("This is my element . click to edit");
 divElem.appendChild(text);
-console.log(divElem);
-
 divElem.setAttribute("class", "divelem");
 divElem.setAttribute("id", "divelem");
+let container = document.querySelector(".container");
+let first = document.getElementById("myfirst");
+console.log(divElem);
+
+divElem.setAttribute(
+  "style",
+  "border:2px solid black; width:154px; margin:34px; padding:23px"
+);
+
+// insert the element
+container.insertBefore(divElem, first);
