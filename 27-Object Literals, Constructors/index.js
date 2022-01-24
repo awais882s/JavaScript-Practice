6154e58c77d9c50c6fc61ca46e53942a5911f22e
Muhammad Awais <awaisiqbal78692110@gmail.com>
2022-01-24 16:47:23 +0500

    function of car

--- 27-Object Literals, Constructors/index.js
+++ 27-Object Literals, Constructors/index.js
@@ -1,2 +1,10 @@
 console.log("Object Literals, Constructors");
-
+// object literals for creating objects
+let car = {
+  name: "Corolla",
+  topSpeed: 89,
+  run: function () {
+    console.log("car is running");
+  },
+};
+console.log(car);
