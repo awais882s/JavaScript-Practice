73d1777ef0c40d469015d6d2d5b9f5f5b11ce5f8
Muhammad Awais <awaisiqbal78692110@gmail.com>
2022-01-24 17:14:25 +0500

    create diff functions

--- 27-Object Literals, Constructors/index.js
+++ 27-Object Literals, Constructors/index.js
@@ -8,3 +8,18 @@ let car = {
   },
 };
 console.log(car);
+// we have already seen constructor like this
+// new Date();
+
+// creating a constructor for cars
+function GeneralCar(givenName, givenSpeed) {
+  this.name = givenName;
+  this.topSpeed = givenSpeed;
+  this.run = function () {
+    console.log(`${this.name}is running`);
+  };
+}
+car1 = new GeneralCar("Nissan", 180);
+car2 = new GeneralCar("AutoCar", "200km");
+console.log(car1);
+console.log(car2);
