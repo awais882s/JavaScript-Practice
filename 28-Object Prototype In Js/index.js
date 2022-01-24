166896a5b9856b01b40901f458429995b3d3e966
Muhammad Awais <awaisiqbal78692110@gmail.com>
2022-01-24 22:26:58 +0500

    Update index.js

--- 28-Object Prototype In Js/index.js
+++ 28-Object Prototype In Js/index.js
@@ -1,1 +1,18 @@
 console.log("Object Prototype In javascript");
+// object  : object.prototype
+let obj1 = {
+  name: "awais",
+  channel: "muhammad awais",
+  adress: "Arifwala",
+};
+function obj(givenName) {
+  this.name = givenName;
+}
+obj.prototype.getName = function () {
+  return this.name;
+};
+
+
+
+let obj2 = new obj("Chasmato");
+console.log(obj2.toString);
