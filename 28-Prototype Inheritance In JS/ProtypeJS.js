22a689caabd7ed4c9eaf619bf91325d3df700570
Muhammad Awais <awaisiqbal78692110@gmail.com>
2022-01-25 11:00:24 +0500

    Update ProtypeJS.js

--- 28-Prototype Inheritance In JS/ProtypeJS.js
+++ 28-Prototype Inheritance In JS/ProtypeJS.js
@@ -7,15 +7,37 @@ const proto = {
     this.name = newName;
   },
 };
+// 1st methods to create prototype
+// const awais = Object.create(proto);
 
-const awais = Object.create(proto);
+// awais.name = "awais";
+// awais.role = "programmer";
+// awais.changeName("Awais882s");
 
-awais.name = "awais";
-awais.role = "programmer";
-awais.changeName("Awais882s");
-
-
+// 2nd way to create prototype
 const awais = Object.create(proto, {
   name: { value: "awais22" },
   role: { value: "lover and Developers" },
 });
+
+console.log(awais);
+
+// ==========================================================================
+
+// Employe constructor
+function Employee(name, salary, expr) {
+  this.name = name;
+  this.salary = salary;
+  this.expr = expr;
+}
+
+// slogan
+
+Employee.prototype.slogan = function () {
+  return `this company is the best. regards ${this.name}`;
+};
+
+// create objects
+
+let awaisObj = new Employee("awais", 34000, "3years");
+console.log(awaisObj.slogan());
