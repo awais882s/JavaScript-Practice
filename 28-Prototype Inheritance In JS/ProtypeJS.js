3953a2d6a2813f3297a45ba793549d4d4f781e37
Muhammad Awais <awaisiqbal78692110@gmail.com>
2022-01-25 10:51:43 +0500

    Update ProtypeJS.js

--- 28-Prototype Inheritance In JS/ProtypeJS.js
+++ 28-Prototype Inheritance In JS/ProtypeJS.js
@@ -1,1 +1,21 @@
 console.log("Prototype Inheritance In JavaScrip");
+const proto = {
+  slogan: function () {
+    return `this company is the best`;
+  },
+  changeName: function (newName) {
+    this.name = newName;
+  },
+};
+
+const awais = Object.create(proto);
+
+awais.name = "awais";
+awais.role = "programmer";
+awais.changeName("Awais882s");
+
+
+const awais = Object.create(proto, {
+  name: { value: "awais22" },
+  role: { value: "lover and Developers" },
+});
