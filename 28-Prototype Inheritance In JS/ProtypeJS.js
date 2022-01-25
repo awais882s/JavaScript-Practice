9b1b9b6f91ff3c855f2ad68da01f2b12e4204547
Muhammad Awais <awaisiqbal78692110@gmail.com>
2022-01-25 11:14:01 +0500

    Update ProtypeJS.js

--- 28-Prototype Inheritance In JS/ProtypeJS.js
+++ 28-Prototype Inheritance In JS/ProtypeJS.js
@@ -8,17 +8,17 @@ const proto = {
   },
 };
 // 1st methods to create prototype
-// const awais = Object.create(proto);
+const awais = Object.create(proto);
 
-// awais.name = "awais";
-// awais.role = "programmer";
-// awais.changeName("Awais882s");
+awais.name = "awais";
+awais.role = "programmer";
+awais.changeName("Awais882s");
 
 // 2nd way to create prototype
-const awais = Object.create(proto, {
-  name: { value: "awais22" },
-  role: { value: "lover and Developers" },
-});
+// const awais = Object.create(proto, {
+//   name: { value: "awais22" },
+//   role: { value: "lover and Developers" },
+// });
 
 console.log(awais);
 
@@ -41,3 +41,19 @@ Employee.prototype.slogan = function () {
 
 let awaisObj = new Employee("awais", 34000, "3years");
 console.log(awaisObj.slogan());
+
+// Programmer
+
+function Programmer(name, salary, experience, language) {
+  Employee.call(this, name, salary, experience);
+  this.language = language;
+}
+
+// Inherit the prototype
+
+Programmer.prototype = Object.create(Employee.prototype);
+// Manually set the constructor
+Programmer.prototype.consstructor = Programmer;
+
+let Chasmato = new Programmer("chasmato", "10", 3, "Javascript");
+console.log(Chasmato);
