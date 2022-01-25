ae1790fad34b509bb5ef6e88d8739eac277f2a65
Muhammad Awais <awaisiqbal78692110@gmail.com>
2022-01-25 15:14:43 +0500

    for flour

--- 28-Prototype Inheritance In JS/Another Example/Floor.js
+++ 28-Prototype Inheritance In JS/Another Example/Floor.js
@@ -1,1 +1,24 @@
-console.log("Floor");
+console.log("Flour");
+// Floor Constructor
+function Flour(egg, bakingpowder, whitesugar) {
+  this.egg = egg;
+  this.bakingpowder = bakingpowder;
+  this.whitesugar = whitesugar;
+}
+// slogan
+Flour.prototype.slogan = function () {
+  return `This cake is best`;
+};
+
+// create an object from this constructor now
+
+let make = new Flour(2, 500, 7);
+console.log(make.slogan());
+
+// cake
+
+function cake(egg, bakingpowder, shitesugar, water, over) {
+  Flour.call(this, egg, bakingpowder, whitesugar);
+  this.water = water;
+  this.over = oven;
+}
