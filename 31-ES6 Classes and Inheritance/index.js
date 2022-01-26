20ff3067facfa35c98684e4adb1c6326e7fa9439
Muhammad Awais <awaisiqbal78692110@gmail.com>
2022-01-26 23:18:45 +0500

    constructor examples

--- 31-ES6 Classes and Inheritance/index.js
+++ 31-ES6 Classes and Inheritance/index.js
@@ -78,11 +78,53 @@ function getMobile(manufacturer, model, year) {
   };
 }
 getMobile("Samsung", "Galaxy", "2020");
-//   Promises
 
+//   Promises
 var asyncCall = new Promise((resolve, reject) => {
   // do something
   resolve();
 }).then(() => {
   console.log("DON!");
 });
+
+// clases
+
+class UserProfile {
+  constructor(firstName, lastName) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+
+  getName() {
+    console.log(`The Full-Name is ${this.firstName} ${this.lastName}`);
+  }
+}
+let obj = new UserProfile("John", "Smith");
+obj.getName(); // output: The Full-Name is John Smith
+
+// ANother Examples
+class YourLovePersonsName {
+  constructor(first_name, Nick_Name) {
+    this.firstName = first_name;
+    this.NickName = Nick_Name;
+  }
+  getName() {
+    console.log(
+      `Your Love ${this.firstName} and Your NickName is ${this.NickName}`
+    );
+  }
+}
+
+let AWAIS = new YourLovePersonsName("AWAIS_S", "Shahzady_gi");
+console.log(AWAIS.getName());
+
+class Lover extends YourLovePersonsName {
+  constructor(first_name, Nick_Name, age, loveAbberivation) {
+    super(first_name, Nick_Name, age, loveAbberivation);
+    this.age = age;
+    this.loveAbberivation = loveAbberivation;
+  }
+}
+
+let KAMRAN = new Lover("Kamran_Gujjar","KamiBhai",20,"S");
+console.log(KAMRAN);
