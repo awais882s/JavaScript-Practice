5c78ac3124fd198d5f98e83cb1b28c333a308e77
Muhammad Awais <awaisiqbal78692110@gmail.com>
2022-01-26 22:07:54 +0500

    constructor

--- 31-ES6 Classes and Inheritance/index.js
+++ 31-ES6 Classes and Inheritance/index.js
@@ -1,1 +1,13 @@
 console.log("ES6 Classes and Inheritance");
+
+class Employee {
+  constructor(givenName, givenExperience, givenDivision) {
+    this.name = givenName;
+    this.experience = givenExperience;
+    this.division = givenDivision;
+  }
+}
+
+awais = new Employee("awais", 50, "Division");
+console.log(awais);
+
