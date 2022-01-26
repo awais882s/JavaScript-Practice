4b5172024d989efd353f2c87422ecbcda6f2fdd0
Muhammad Awais <awaisiqbal78692110@gmail.com>
2022-01-26 22:15:31 +0500

    Update index.js

--- 31-ES6 Classes and Inheritance/index.js
+++ 31-ES6 Classes and Inheritance/index.js
@@ -9,8 +9,16 @@ class Employee {
   slogan() {
     return `i am ${this.name} this company is best`;
   }
+  joiningYear() {
+    return 2020 - this.experience;
+  }
+  static add(a, b) {
+    return a + b;
+  }
 }
 
-awais = new Employee("awais", 50, "Division");
-console.log(awais);
+awais = new Employee("awais", 56, "Division");
+console.log(awais.joiningYear());
+
+console.log(Employee.add(34, 5));
 
