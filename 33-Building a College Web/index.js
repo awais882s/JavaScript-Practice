30dc5fc6e1ecce8268869a87afb27a2a1bf61349
Muhammad Awais <awaisiqbal78692110@gmail.com>
2022-01-28 23:00:00 +0500

    constructor

--- 33-Building a College Web/index.js
+++ 33-Building a College Web/index.js
@@ -1,1 +1,17 @@
 console.log("Building a College Library Website ");
+
+// constructor
+
+function Book(name, author, type) {
+  this.name = name;
+  this.author = author;
+  this.type = type;
+}
+
+// Display constructor
+
+function Display() {
+
+    
+}
+//  add methods to display prototype
