fb7eb94c3a4f8b60816cf0de63d0b5e485801580
Muhammad Awais <awaisiqbal78692110@gmail.com>
2022-02-01 22:43:40 +0500

    Update index.js

--- 34-Async and Sync/index.js
+++ 34-Async and Sync/index.js
@@ -1,1 +1,8 @@
 console.log("Async and Sync");
+for (let index = 0; index < 10000; index++) {
+  const element = index;
+  console.log(element);
+  console.log("This is index number " + index);
+}
+
+console.log("Done Printing");
