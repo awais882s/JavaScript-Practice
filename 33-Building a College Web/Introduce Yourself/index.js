d69257c34999ada063a868508756630a14750a6e
Muhammad Awais <awaisiqbal78692110@gmail.com>
2022-02-01 10:44:19 +0500

    add event listeners on submit

--- 33-Building a College Web/Introduce Yourself/index.js
+++ 33-Building a College Web/Introduce Yourself/index.js
@@ -1,2 +1,27 @@
 console.log("My personal information");
 
+// constructor
+function Information(name, city, province) {
+  this.name = name;
+  this.city = city;
+  this.province = province;
+}
+
+// add submit event listeners on InformationForm
+
+let InformationForm = document.getElementById("InformationForm");
+InformationForm.addEventListener("submit", InformationFormSubmit);
+function InformationFormSubmit(e) {
+  let name = document.getElementById("name");
+  let city = document.getElementById("city");
+
+  let punjab = document.getElementById("punjab");
+  let sindh = document.getElementById("sindh");
+  let kashmir = document.getElementById("kashmir");
+  let province;
+  if (punjab.checked) {
+    province = punjab.value;
+  }
+  //   console.log("Thanks for Submit Your Data");
+  e.preventDefault();
+}
