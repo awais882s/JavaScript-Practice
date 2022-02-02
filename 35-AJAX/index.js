7a9b03dd1be06ee3838b594bc4d52df91c102969
Muhammad Awais <awaisiqbal78692110@gmail.com>
2022-02-02 15:24:29 +0500

    get method

--- 35-AJAX/index.js
+++ 35-AJAX/index.js
@@ -8,17 +8,27 @@ function buttonClickHandler() {
   const xhr = new XMLHttpRequest();
 
   //   open the object
-  xhr.open("GET", "awais.txt", true);
+  //   xhr.open("GET", "awais.txt", true); for get request
+  xhr.open("GET", "https://jsonplaceholder.typicode.com/todos/1", true);
 
   //   what to do on progress (optional)
   xhr.onprogress = function () {
     console.log("On Progress");
   };
 
+  //   xhr.onreadystatechange = function () {
+  //     console.log("ready state is", xhr.readyState);
+  //   };
+
   //   what to do when response is ready
   xhr.onload = function () {
-    console.log(this.responseText);
+    if (this.status === 200) {
+      console.log(this.responseText);
+    } else {
+      console.log("Some error ocurred");
+    }
   };
   //   send the request
   xhr.send();
+  console.log("We are Done");
 }
