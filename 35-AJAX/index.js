6050dc69635791fbb88f85d9871c1488a7a7f35d
Muhammad Awais <awaisiqbal78692110@gmail.com>
2022-02-02 14:07:48 +0500

    xhr diff func

--- 35-AJAX/index.js
+++ 35-AJAX/index.js
@@ -1,1 +1,22 @@
 console.log("AJAX");
+
+let fetchBtn = document.getElementById("fetchBtn");
+fetchBtn.addEventListener("click", buttonClickHandler);
+function buttonClickHandler() {
+  console.log("You have Clicked the fetchBtn");
+  // instantiate an xhr object
+  const xhr = new XMLHttpRequest();
+
+  //   open the object
+  xhr.open("GET", "harry.txt", true);
+
+  //   what to do on progress (optional)
+  xhr.onprogress = function () {
+    console.log("On Progress");
+  };
+
+  //   what to do when response is ready
+  xhr.onload = function () {
+    console.log(this.responseText);
+  };
+}
