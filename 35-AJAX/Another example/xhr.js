60685d5c1e3421521c94958b2590e472e75891bf
Muhammad Awais <awaisiqbal78692110@gmail.com>
2022-02-03 10:07:18 +0500

    add event listeners

--- 35-AJAX/Another example/xhr.js
+++ 35-AJAX/Another example/xhr.js
@@ -1,1 +1,8 @@
 console.log("XHR Request(HTTP REQUEST)");
+
+let btn = document.getElementById("btn");
+btn.addEventListener("click", primaryBtn);
+function primaryBtn() {
+  console.log("You have clicked the Primary Btn");
+  //   intiate and xhr open
+}
