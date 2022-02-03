b816d43a60ea3040875b3ad4542ed056216d73c1
Muhammad Awais <awaisiqbal78692110@gmail.com>
2022-02-03 10:36:23 +0500

    secondary btn

--- 35-AJAX/Another example/xhr.js
+++ 35-AJAX/Another example/xhr.js
@@ -23,3 +23,23 @@ function primaryBtn() {
   console.log("We are DONE!");
 }
 // another btn
+let btn1 = document.getElementById("btn1");
+btn1.addEventListener("click", secondaryBtn);
+function secondaryBtn() {
+  console.log("You have created the secondary Btn");
+  const xhr = new XMLHttpRequest();
+  //   xhr.open("GET", "xhr.txt", true);
+  xhr.open("POST", "http://dummy.restapiexample.com/api/v1/create", true);
+  xhr.onprogress = function () {
+    console.log("Your secondary btn is loaded");
+  };
+  xhr.onload = function () {
+    if (this.status === 200) {
+      console.log(this.responseText);
+    } else {
+      console.log("Your file is missings");
+    }
+  };
+  xhr.send();
+  console.log("your http request for secondary btn is done");
+}
