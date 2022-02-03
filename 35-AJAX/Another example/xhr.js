12ee590a4e301d1efa799dde85c43d486783f7bd
Muhammad Awais <awaisiqbal78692110@gmail.com>
2022-02-03 10:21:58 +0500

    on primary btn

--- 35-AJAX/Another example/xhr.js
+++ 35-AJAX/Another example/xhr.js
@@ -5,4 +5,21 @@ btn.addEventListener("click", primaryBtn);
 function primaryBtn() {
   console.log("You have clicked the Primary Btn");
   //   intiate and xhr open
+  const xhr = new XMLHttpRequest();
+  //   xhr.open("GET", "xhr.txt", true);
+  xhr.open("POST", "http://dummy.restapiexample.com/api/v1/create", true);
+
+  xhr.onprogress = function () {
+    console.log("Your Progress is Running");
+  };
+  xhr.onload = function () {
+    if (this.status == 200) {
+      console.log(this.responseText);
+    } else {
+      console.log("Some error occured");
+    }
+  };
+  xhr.send();
+  console.log("We are DONE!");
 }
+// another btn
