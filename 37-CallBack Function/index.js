28bb5bc342c7b022b062a05d4a623a91c94d4f5f
Muhammad Awais <awaisiqbal78692110@gmail.com>
2022-02-04 11:43:27 +0500

    create files

---
+++ 37-CallBack Function/index.js
@@ -0,0 +1,6 @@
+console.log("CallBack Function");
+// pretend that this response is coming from the server
+const students = [
+  { name: "awais", subject: "JS" },
+  { name: "Razzaq", subject: "SE" },
+];
