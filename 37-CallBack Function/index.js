73c159c32905aa55e5ee51aff557f32c6ff209d5
Muhammad Awais <awaisiqbal78692110@gmail.com>
2022-02-04 12:18:27 +0500

    Update index.js

--- 37-CallBack Function/index.js
+++ 37-CallBack Function/index.js
@@ -5,11 +5,12 @@ const students = [
   { name: "Razzaq", subject: "SE" },
 ];
 
-function enrollStudent(student) {
+function enrollStudent(student, callback) {
   setTimeout(function () {
     students.push(student);
     console.log("Student has been enrolled");
-  }, 8000);
+    callback();
+  }, 1000);
 }
 function getStudent() {
   setTimeout(function () {
@@ -19,8 +20,8 @@ function getStudent() {
     });
     document.getElementById("students").innerHTML = str;
     console.log("Student has been fethched");
-  }, 1000);
+  }, 5000);
 }
 let newStudent = { name: "waqar", subject: "python" };
-enrollStudent(newStudent);
-getStudent();
+enrollStudent(newStudent, getStudent);
+// getStudent();
