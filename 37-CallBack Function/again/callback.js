1bb965d348e12d1c50da594fe807d4ce113d622b
Muhammad Awais <awaisiqbal78692110@gmail.com>
2022-02-04 15:25:28 +0500

    callBAck func

--- 37-CallBack Function/again/callback.js
+++ 37-CallBack Function/again/callback.js
@@ -1,1 +1,30 @@
 console.log("CallBAck functions");
+
+// this response is coming from the server
+
+const students = [
+  { name: "AWAIS", subjects: "SE" },
+  { name: "WAQAR", subjects: "PYTHON" },
+];
+
+function enrollStudent(student, callback) {
+  setTimeout(function () {
+    students.push(student);
+    console.log("student has been enrollled");
+    callback();
+  }, 3000);
+}
+function getStudents() {
+  setTimeout(function () {
+    let str = "";
+    students.forEach(function (student) {
+      str += `<li>${student.name}</li>`;
+    });
+    document.getElementById("std").innerHTML = str;
+    console.log("student has been fetched");
+  }, 1000);
+}
+
+let newStudent = { name: "RAZZAQ", subjects: "IT" };
+enrollStudent(newStudent, getStudents);
+// getStudents();
