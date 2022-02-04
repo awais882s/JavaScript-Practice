fb438d81d506e167dcd71da9c2209bdc04d8ca4a
Muhammad Awais <awaisiqbal78692110@gmail.com>
2022-02-04 12:09:46 +0500

    callback func

--- 37-CallBack Function/index.js
+++ 37-CallBack Function/index.js
@@ -4,3 +4,21 @@ const students = [
   { name: "awais", subject: "JS" },
   { name: "Razzaq", subject: "SE" },
 ];
+
+function enrollStudent(student) {
+  setTimeout(function () {
+    students.push(student);
+  }, 3000);
+}
+function getStudent() {
+  setTimeout(function () {
+    let str = "";
+    students.forEach(function (student) {
+      str += `<li>${student.name}</li>`;
+    });
+    document.getElementById("students").innerHTML = str;
+  }, 1000);
+}
+let newStudent = {name:"waqar",subject:"python"}
+enrollStudent(newStudent);
+getStudent();
