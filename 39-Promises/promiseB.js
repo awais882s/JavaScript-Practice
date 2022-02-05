c85dc13c89bd8963c2f2779e8a41032871a3df6a
Muhammad Awais <awaisiqbal78692110@gmail.com>
2022-02-05 10:20:14 +0500

    resolve and reject

---
+++ 39-Promises/promiseB.js
@@ -0,0 +1,45 @@
+console.log("CallBAck functions");
+
+// this response is coming from the server
+
+const students = [
+  { name: "AWAIS", subjects: "SE" },
+  { name: "WAQAR", subjects: "PYTHON" },
+];
+
+function enrollStudent(student) {
+  return new Promise(function (resolve, reject) {
+    setTimeout(function () {
+      students.push(student);
+      console.log("student has been enrollled");
+      resolve();
+      const error = false;
+      if (!error) {
+        resolve();
+      } else {
+        reject();
+      }
+    }, 3000);
+  });
+}
+function getStudents() {
+  setTimeout(function () {
+    let str = "";
+    students.forEach(function (student) {
+      str += `<li>${student.name}</li>`;
+    });
+    document.getElementById("std").innerHTML = str;
+    console.log("student has been fetched");
+  }, 1000);
+}
+
+let newStudent = { name: "RAZZAQ", subjects: "IT" };
+enrollStudent(newStudent)
+  .then(function () {
+    getStudents();
+  })
+  .catch(function () {
+    console.log("some error occured");
+  });
+// getStudents();
+
