6c556a6be325ec5204bf13adb0f2a901450ce259
Muhammad Awais <awaisiqbal78692110@gmail.com>
2022-02-05 12:40:31 +0500

    diff arrow

--- 40-Arrow Function/arrow.js
+++ 40-Arrow Function/arrow.js
@@ -1,12 +1,27 @@
 console.log("Arrow Function");
-function awais() {
-  console.log("This is the best perosn ever");
-}
+// Arrow functions
 
-awais();
+// create a normal function
+// function awais() {
+//   console.log("This is the best perosn ever");
+// }
 
+// awais();
+
+// convert into arrow function
 const awais1 = () => {
   console.log("THis is my arrow function");
 };
-
 awais1();
+
+// const greet = function () {
+//   return " Good Morning ";
+// };
+
+// console.log(greet());
+
+
+// one liners do not require/braces
+// one line will automatically return 
+const greet = () => "good morning awais ";
+console.log(greet());
