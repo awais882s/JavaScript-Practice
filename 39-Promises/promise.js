a342f053b97fb5b76e4906499c53064d4c76b26a
Muhammad Awais <awaisiqbal78692110@gmail.com>
2022-02-05 10:02:37 +0500

    resolve and reject

--- 39-Promises/promise.js
+++ 39-Promises/promise.js
@@ -1,2 +1,29 @@
 console.log("Promises");
 
+// Promise : Best video on promise
+// - resolve
+// -reject
+// -pending
+
+function func1() {
+  return new Promise(function (resolve, reject) {
+    setTimeout(() => {
+      const error = true;
+      if (!error) {
+        console.log("Function : Your promise is resolved");
+        resolve();
+      } else {
+        console.log(" Function : Your promise is reject");
+        reject("soory not fulfilled");
+      }
+    }, 2000);
+  });
+}
+
+func1()
+  .then(function () {
+    console.log(" Awais Thanks for resolving");
+  })
+  .catch(function () {
+    console.log("Awais : very bad bro");
+  });
