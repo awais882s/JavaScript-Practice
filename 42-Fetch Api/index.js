da2826dc7723acb0de32ae1329fbe7ce456d084b
Muhammad Awais <awaisiqbal78692110@gmail.com>
2022-02-06 22:55:36 +0500

    func getData

--- 42-Fetch Api/index.js
+++ 42-Fetch Api/index.js
@@ -2,8 +2,16 @@ console.log("Fetch Api");
 // button with id myBtn
 let myBtn = document.getElementById("myBtn");
 
-
-
 // div with id content
 let content = document.getElementById("content");
 
+function getData() {
+  url = awais.txt;
+  fetch(url)
+    .then((response) => {
+      return response.text();
+    })
+    .then(() => {
+      console.log(data);
+    });
+}
